59ebb5c1c19ba6ac4b8e3401278e3c03f198480a
Xavier Flix <morphx666@users.noreply.github.com>
2022-02-18 18:13:14 -0400

    Implemented PURGE opcode

---
+++ RPN/OpCodes/Memory/Purge.cs
@@ -0,0 +1,28 @@
+﻿using RPN.OpCodes.Logic;
+using System.Collections.Generic;
+
+namespace RPN.OpCodes.Memory {
+    [OpCodeAttr(nameof(Purge))]
+    internal class Purge : OpCode {
+        public Purge() {
+            ArgumentCount = 1;
+            Symbols = new string[] { "PURGE" };
+            DataTypes = new Types[] { Types.Infix };
+            Associativity = Associativities.Left;
+            Precedence = 5;
+        }
+
+        public override bool ExecuteInternal(RPNStack rpn, Types dataType) {
+            string v1 = rpn.Pop().Token;
+
+            foreach(RPNStack.Variable v in rpn.Variables) {
+                if(v.Name == v1) {
+                    rpn.Variables.Remove(v);
+                    break;
+                }
+            }
+
+            return true;
+        }
+    }
+}
